3c6ed3fb4aaf9cb8a479e4f08e7668a4b2e8b7e0
LarsingDash <l.villevoye@student.avans.nl>
2022-02-10 16:04:40 +0100

    Added Schedule table

---
+++ src/Gui/ScheduleCell.java
@@ -0,0 +1,47 @@
+package Gui;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+
+public class ScheduleCell extends Pane {
+    //For all cells with text
+    public ScheduleCell(String text, int type, boolean isLeft) {
+        this.setStyle(type, isLeft);
+
+        Label label = new Label(text);
+        label.setFont(new Font(35));
+        label.layoutXProperty().bind(this.widthProperty().subtract(label.widthProperty()).divide(2));
+        this.getChildren().add(label);
+    }
+
+    //For empty cells
+    public ScheduleCell(int type, boolean isLeft) {
+        this.setStyle(type, isLeft);
+    }
+
+    private void setStyle(int type, boolean isLeft) {
+        String border = "-fx-border-width: 1; -fx-border-style: solid";
+
+        //Background and border of cell
+        switch (type) {
+            case 0:
+                super.setStyle(border);
+                break;
+
+            case 1:
+                super.setStyle("-fx-background-color: #d9d9d9 ;" + border);
+                break;
+
+            case 2:
+                super.setStyle("-fx-background-color: #b4b4b4 ;" + border);
+        }
+
+        //Size of cell
+        if (isLeft) {
+            this.setPrefSize(300, 50);
+        } else {
+            this.setPrefSize(215, 50);
+        }
+    }
+}
--- src/Gui/ScheduleView.java
+++ src/Gui/ScheduleView.java
@@ -1,9 +1,62 @@
 package Gui;
 
-import javafx.scene.canvas.Canvas;
+import javafx.geometry.Pos;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
-public class ScheduleView extends Canvas {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ScheduleView extends BorderPane {
+    private final GridPane scheduleGridPane = new GridPane();
+
     public ScheduleView () {
-        super(1920, 1080);
+        super.setPrefSize(1920, 1080);
+        this.setCenter(this.scheduleGridPane);
+        this.scheduleGridPane.setAlignment(Pos.CENTER);
+
+        this.buildSchedule();
+    }
+
+    private void buildSchedule () {
+        //Top row
+        this.scheduleGridPane.addRow(0,
+                new ScheduleCell("Class block / Time", 2, true),
+                new ScheduleCell("Classroom 1", 2, false),
+                new ScheduleCell("Classroom 2", 2, false),
+                new ScheduleCell("Classroom 3", 2, false),
+                new ScheduleCell("Classroom 4", 2, false),
+                new ScheduleCell("Classroom 5", 2, false),
+                new ScheduleCell("Classroom 6", 2, false));
+
+        //Left column
+        //Generate times
+        ArrayList<String> times = new ArrayList<>();
+        Collections.addAll(times,
+                ("1  / 08:00 - 09:00"),
+                ("2  / 09:00 - 10:00"),
+                ("3  / 10:00 - 11:00"),
+                ("4  / 11:00 - 12:00"),
+                ("5  / 12:30 - 13:30"),
+                ("6  / 13:30 - 14:30"),
+                ("7  / 14:30 - 15:30"),
+                ("8  / 16:00 - 17:00"),
+                ("9  / 17:00 - 18:00"),
+                ("10 / 18:00 - 19:00"));
+
+        //Generate rows: time + 6 blank cells
+        int rowIndex = 0;
+        int type = 1;
+        for (String time : times) {
+            type = (type + 1) % 2;
+            rowIndex++;
+
+            int columnIndex = 0;
+            this.scheduleGridPane.add(new ScheduleCell(time, type, true), columnIndex, rowIndex);
+
+            for (columnIndex++; columnIndex < 7; columnIndex++) {
+                this.scheduleGridPane.add(new ScheduleCell(type, false), columnIndex, rowIndex);
+            }
+        }
     }
 }
