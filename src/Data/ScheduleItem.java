dc21dd2991205927ee4ed97928888254c709fc79
Lucas <29230467+Somfic@users.noreply.github.com>
2022-02-10 13:24:19 +0100

    Use ArrayList instead of Array

--- src/Data/ScheduleItem.java
+++ src/Data/ScheduleItem.java
@@ -1,22 +1,25 @@
 package Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScheduleItem {
 	private Teacher teacher;
 	private StudentGroup Class;
 	private Classroom Classroom;
 	private int startPeriod;
 	private int endPeriod;
-	private StudentGroup[] studentGroup;
+	private List<StudentGroup> studentGroup;
 	private Classroom classroom;
 	private Lesson lesson;
 
-    public ScheduleItem(Teacher teacher, StudentGroup aClass, Data.Classroom classroom, int startPeriod, int endPeriod, StudentGroup[] studentGroup, Data.Classroom classroom1, Lesson lesson) {
+    public ScheduleItem(Teacher teacher, StudentGroup aClass, Data.Classroom classroom, int startPeriod, int endPeriod, Data.Classroom classroom1, Lesson lesson) {
         this.teacher = teacher;
         Class = aClass;
         Classroom = classroom;
         this.startPeriod = startPeriod;
         this.endPeriod = endPeriod;
-        this.studentGroup = studentGroup;
+        this.studentGroup = new ArrayList<>();
         this.classroom = classroom1;
         this.lesson = lesson;
     }
