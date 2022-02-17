d0635716113ba16bd273785c19070601bd3ede38
Owen <owen.arts@hotmail.com>
2022-02-17 15:16:07 +0100

    Added labels to Setting Screen

--- src/Gui/SettingsScreen/SettingView.java
+++ src/Gui/SettingsScreen/SettingView.java
@@ -10,6 +10,9 @@ import javafx.scene.paint.Color;
 public class SettingView implements SpeedSelectorCallback, ColorCallback, ClassBlockCallback {
     private BorderPane borderPane;
     private Label titleLabel;
+    private Label speedLabel;
+    private Label colorSelectorLabel;
+    private Label classBlockLabel;
     private SpeedSelector speedSelector;
     private ColorSelector colorSelector;
     private ClassBlock classBlock;
@@ -20,10 +23,13 @@ public class SettingView implements SpeedSelectorCallback, ColorCallback, ClassB
     public SettingView() {
         this.borderPane = new BorderPane();
         this.titleLabel = new Label("Settings");
+        this.speedLabel = new Label("Simulator Speed");
+        this.colorSelectorLabel = new Label("Theme Color");
+        this.classBlockLabel = new Label("Class Block Lenth (in minutes)");
         this.speedSelector = new SpeedSelector(this);
         this.colorSelector = new ColorSelector(this);
         this.classBlock = new ClassBlock(this);
-        this.centralPane = new VBox(speedSelector.getContent(), colorSelector.getContent(), classBlock.getContent(), new HBox(confirm, cancel));
+        this.centralPane = new VBox(speedLabel, speedSelector.getContent(),colorSelectorLabel, colorSelector.getContent(), classBlockLabel, classBlock.getContent(), new HBox(confirm, cancel));
         this.centralPane.setFillWidth(true);
         this.centralPane.setSpacing(15);
         
