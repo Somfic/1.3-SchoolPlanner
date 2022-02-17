f171704383604e47ee5a26ccc3c479ad2da50c3a
Lucas <29230467+Somfic@users.noreply.github.com>
2022-02-17 15:48:33 +0100

    Fixed buttons below schedule

--- src/Gui/ScheduleView.java
+++ src/Gui/ScheduleView.java
@@ -17,12 +17,6 @@ public class ScheduleView extends Pane {
     private GridPane scheduleGridPane = new GridPane();
     private Schedule schedule = new Schedule();
     private int width;
-    private GridPane grid;
-    private Button save = new Button("Save");
-    private Button load = new Button("Load");
-    private Button apply = new Button("Apply");
-    private Button reset = new Button("Reset");
-    private Button remove = new Button("Remove");
 
     public ScheduleView () {
         //super.setPrefSize(1920, 1080);
@@ -31,29 +25,6 @@ public class ScheduleView extends Pane {
 
         this.TESTMETHOD();
         this.buildScheduleTable();
-        grid = new GridPane();
-        grid.add(apply, 0, 0);
-        grid.add(reset, 1, 1);
-        grid.add(remove, 2, 0);
-        grid.add(save, 3, 0);
-        grid.add(load, 3, 1);
-
-        apply.setOnAction(event -> {
-            //button pressed
-        });
-        reset.setOnAction(event -> {
-            //button pressed
-        });
-        remove.setOnAction(event -> {
-            //button pressed
-        });
-        save.setOnAction(event -> {
-            //button pressed
-        });
-        load.setOnAction(event -> {
-            //button pressed
-        });
-
     }
 
     private void TESTMETHOD() {
