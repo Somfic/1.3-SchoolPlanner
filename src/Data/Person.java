7a39b7ac520c2bbe96aff4e2f9aa9b51b52a61af
LarsingDash <l.villevoye@student.avans.nl>
2022-02-22 18:12:39 +0100

    Can now select a scheduleItem
    Added Person and Lesson toString()

--- src/Data/Person.java
+++ src/Data/Person.java
@@ -28,4 +28,9 @@ public abstract class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
