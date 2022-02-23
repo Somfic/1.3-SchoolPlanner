65a07730f52cbb7dae900fc8d2ce1ef16f186f41
Quincy <qbeerman@gmail.com>
2022-02-23 20:33:57 +0100

    Fixed classroom indexes

--- src/Gui/SelectButtons.java
+++ src/Gui/SelectButtons.java
@@ -43,7 +43,7 @@ public class SelectButtons extends Pane {
         this.teacherSelect.setPromptText("Teacher  ");
         this.courseSelect.setPromptText("Course     ");
 
-        this.classRoomSelect.getItems().addAll(new Classroom(30, "Classroom 1", 0), new Classroom(30, "Classroom 2", 0), new Classroom(30, "Classroom 3", 0), new Classroom(30, "Classroom 4", 0), new Classroom(30, "Classroom 5", 0), new Classroom(30, "Classroom 6", 0));
+        this.classRoomSelect.getItems().addAll(new Classroom(30, "Classroom 1", 0), new Classroom(30, "Classroom 2", 1), new Classroom(30, "Classroom 3", 2), new Classroom(30, "Classroom 4", 3), new Classroom(30, "Classroom 5", 4), new Classroom(30, "Classroom 6", 5));
 
 
         this.buildStudentGroups(new ArrayList<>());
