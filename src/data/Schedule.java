1730b38fc1be35ceee9e344f4c3497b5fea33016
Owen <owen.arts@hotmail.com>
2022-04-04 19:45:45 +0200

    Removed overlapping conflict

--- src/data/Schedule.java
+++ src/data/Schedule.java
@@ -1,35 +1,51 @@
 package data;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import logging.Logger;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.media.jfxmediaimpl.HostUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Schedule {
     private final List<ScheduleItem> items;
-
     public Schedule() {
         this.items = new ArrayList<>();
     }
 
     public void add(ScheduleItem item) {
         validate(item);
-        if (item.getEndPeriod() - item.getStartPeriod() >= 0 && item.getEndPeriod() >= 1 && item.getEndPeriod() <= 10 && item.getStartPeriod() >= 1 && item.getStartPeriod() <= 10) {
+        if(item.getEndPeriod() - item.getStartPeriod() >= 0 && item.getEndPeriod() >= 1 && item.getEndPeriod()<= 10 &&  item.getStartPeriod() >= 1 && item.getStartPeriod() <= 10){
             items.add(item);
         }
     }
 
     private void validate(ScheduleItem item) {
-        for (int i = 0; i < items.size(); i++) {
-            if (item.getClassroom().getName().equals(items.get(i).getClassroom().getName())) {
-                if ((item.getStartPeriod() >= items.get(i).getStartPeriod() && item.getStartPeriod() <= items.get(i).getEndPeriod())
-                        || (item.getEndPeriod() >= items.get(i).getStartPeriod() && item.getEndPeriod() <= items.get(i).getEndPeriod())) {
+        for (int i = 0; i <items.size(); i++) {
+            if (overlapping(item, items.get(i))) {
+                // students
+                ScheduleItem compareItem = items.get(i);
+                ArrayList<StudentGroup> itemStudents = new ArrayList<>(item.getStudentGroups());
+                itemStudents.retainAll(compareItem.getStudentGroups());
+                if (item.getTeacher().equals(compareItem.getTeacher()) || item.getClassroom().equals(compareItem.getClassroom())
+                        || itemStudents.size()!=item.getStudentGroups().size()) {
                     items.remove(i);
-                    validate(item);
                 }
             }
         }
+    }
+
+    private boolean overlapping(ScheduleItem item1, ScheduleItem item2) {
+        if((item1.getStartPeriod() >= item2.getStartPeriod() && item1.getStartPeriod() <= item2.getEndPeriod())
+                || (item1.getEndPeriod() >= item2.getStartPeriod() && item1.getEndPeriod()<=item2.getEndPeriod())) {
+            return true;
+        }
+        return false;
+            /*
+            ((item.getStartPeriod() >= items.get(i).getStartPeriod() && item.getStartPeriod() <= items.get(i).getEndPeriod())
+                        || (item.getEndPeriod() >= items.get(i).getStartPeriod() && item.getEndPeriod() <= items.get(i).getEndPeriod())
+                        || item.getTeacher().equals(items.get(i).getTeacher()) || (items.get(i).getStudentGroups().contains(item.getStudentGroups())))
+             */
     }
 
 
@@ -38,11 +54,9 @@ public class Schedule {
             add(item);
         }
     }
-
-    public void reset() {
+    public void reset(){
         items.clear();
     }
-
     public void remove(ScheduleItem scheduleItem) {
         if (items.size() <= 1) {
             items.clear();
